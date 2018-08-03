ca975f2c1b97f28232c240c54d55d928fc426fb6
Nickolas Yakutovich <nickolas.yakutovich@gameloft.com>
2018-08-03 16:09:39 -0400

    added another button, next step is to draw them on place.

--- state.cpp
+++ state.cpp
@@ -1,8 +1,10 @@
 #include "state.h"
+#include <QDebug>
 
 State::State(QStringList &sl)
 {
     ss = sl;
+    qDebug()<< "state object::" << ss;
     // Complex objects are interpreted during execution time.
     // The variable name is defined in front of it's type specifier.
     // special functions can extract parameters and use it for drawing or changing.
