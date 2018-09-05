c6cdd942457c6e92d88ba7899b4445cda3e744ca
Nickolas Yakutovich <nickolas.yakutovich@gameloft.com>
2018-09-05 16:22:06 -0400

    gcode view class in progress

--- gcodeview.h
+++ gcodeview.h
@@ -3,10 +3,28 @@
 
 #include "layoutobject.h"
 
+
+class LayoutData;
+class MyState;
+
 class GCodeView: public MyLayoutObject
 {
 public:
-    GCodeView();
+    GCodeView(QStringList & sl, LayoutData *ld, int init_state);
+    void drawLayoutObject(QPainter& p, QPoint &loffset);
+    void selectLayoutObject();
+    void setLayoutObjectStatus();
+    void getLayoutObjectStatus();
+    void setLayoutObjectPropertiy();
+    void getLayoutObjectPropertiy();
+    bool isCursorOver(QPoint &pos);
+    bool setState(int state);
+    MyState* getState(LayoutData* ld, QString state_name);
+    QString get_active_state_command();
+
+private:
+    QString  g_code_file;
+
 };
 
 #endif // GCODEVIEW_H
